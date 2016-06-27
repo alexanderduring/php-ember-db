659f8f490b1c2fd75e68c636a0e9652e031537a0
Alexander During <alexanderduring.git@gmail.com>
2016-06-27 20:44:38 +0200

    Added insertMany() to DocumentManager.

--- src/DocumentManager.php
+++ src/DocumentManager.php
@@ -17,18 +17,35 @@ class DocumentManager
 
     public function insert($collectionName, $document)
     {
-        echo "Inserting entry into ".$collectionName.".\n";
+        $this->writeLines($collectionName, array($document));
+    }
 
+
+
+    public function insertMany($collectionName, $documents)
+    {
+        $this->writeLines($collectionName, $documents);
+    }
+    
+    
+
+    private function writeLines($collectionName, $documents)
+    {
         // Open or create file
         $collectionFilePath = $this->getDatabasePath().'/'.$collectionName.'.edb';
         $collectionFileHandle = fopen($collectionFilePath, 'a');
 
-        // Add entry to end of file
-        fwrite($collectionFileHandle, json_encode($document)."\n");
+        // Add entris to end of file
+        foreach ($documents as $document) {
+            fwrite($collectionFileHandle, json_encode($document)."\n");
+        }
+
+        // Close file
         fclose($collectionFileHandle);
-    }
+    }        
 
 
+    
     private function getDatabasePath()
     {
         return $this->config['database']['path'];
