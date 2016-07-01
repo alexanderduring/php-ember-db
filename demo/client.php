f91a594e384e6c3248209c69512dbe9404a80d8a
Alexander During <alexander.during@jobleads.de>
2016-07-01 08:40:45 +0200

    Renamed some variables in command line client.

--- demo/client.php
+++ demo/client.php
@@ -5,7 +5,7 @@ use EmberDb\DocumentManager;
 require_once __DIR__ . '/../src/EmberDb/Document.php';
 require_once __DIR__ . '/../src/EmberDb/DocumentManager.php';
 
-echo "\nEmberDb command line client.\nType your command followed by <return>. Type 'help' to get a command overview or 'quit' to leave the client.\n\n";
+echo "\nEmberDb command line client.\nType your command followed by <return>. Type 'help' to get a command overview or 'exit' to leave the client.\n\n";
 
 $inputStream = fopen('php://stdin', 'r');
 $quit = false;
@@ -13,25 +13,27 @@ $quit = false;
 while (!$quit) {
     echo '> ';
     $inputLine = fgets($inputStream);
-    $command = explode(' ', trim($inputLine));
-    print_r($command);
+    $tokens = explode(' ', trim($inputLine));
+    $command = array_shift($tokens);
+    $parameters = $tokens;
+    print_r($tokens);
 
-    switch ($command[0]) {
+    switch ($command) {
         case 'insert':
-            $collection = $command[1];
-            $document = $command[2];
+            $collection = $parameters[0];
+            $document = $parameters[1];
             echo "You want to insert $document in the $collection collection.\n\n";
             break;
         case 'find':
-            $collection = $command[1];
-            $filter = $command[2];
-            echo "You want to find something in the $command[1] collection that matches $filter.\n\n";
+            $collection = $parameters[0];
+            $filter = $parameters[1];
+            echo "You want to find something in the $collection collection that matches $filter.\n\n";
             break;
-        case 'quit':
+        case 'exit':
             $quit = true;
             echo "Closing EmberDb command line client.\n\n";
             break;
         default:
-            echo "Syntax error: Unknown command '".$command[0]."'.\n\n";
+            echo "Syntax error: Unknown command '".$command."'.\n\n";
     }
 }
