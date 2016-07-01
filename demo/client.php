9083f8303dbc81e2bab6161114301cc12cfe25d9
Alexander During <alexander.during@jobleads.de>
2016-07-01 08:23:14 +0200

    Added some line breaks and a prompt to command line client.

--- demo/client.php
+++ demo/client.php
@@ -5,33 +5,33 @@ use EmberDb\DocumentManager;
 require_once __DIR__ . '/../src/EmberDb/Document.php';
 require_once __DIR__ . '/../src/EmberDb/DocumentManager.php';
 
-print "Type your commands. Type 'quit' on a line by itself when you're done.\n";
+echo "\nEmberDb command line client.\nType your command followed by <return>. Type 'help' to get a command overview or 'quit' to leave the client.\n\n";
 
-$fp = fopen('php://stdin', 'r');
+$inputStream = fopen('php://stdin', 'r');
 $quit = false;
-$message = '';
-while (!$quit) {
-    $next_line = fgets($fp, 1024); // read the special file to get the user input from keyboard
-    if ("quit\n" == $next_line) {
-        $quit = true;
-    } else {
-        $message = trim($next_line);
-        $command = explode(' ', $message);
-        print_r($command);
 
-        switch ($command[0]) {
-            case 'insert':
-                $collection = $command[1];
-                $document = $command[2];
-                echo "You want to insert $document in the $collection collection.\n";
-                break;
-            case 'find':
-                $collection = $command[1];
-                $filter = $command[2];
-                echo "You want to find something in the $command[1] collection that matches $filter.\n";
-                break;
-            default:
-                echo "Syntax error: Unknown command '".$command[0]."'.\n";
-        }
+while (!$quit) {
+    echo '> ';
+    $inputLine = fgets($inputStream);
+    $command = explode(' ', trim($inputLine));
+    print_r($command);
+
+    switch ($command[0]) {
+        case 'insert':
+            $collection = $command[1];
+            $document = $command[2];
+            echo "You want to insert $document in the $collection collection.\n\n";
+            break;
+        case 'find':
+            $collection = $command[1];
+            $filter = $command[2];
+            echo "You want to find something in the $command[1] collection that matches $filter.\n\n";
+            break;
+        case 'quit':
+            $quit = true;
+            echo "Closing EmberDb command line client.\n\n";
+            break;
+        default:
+            echo "Syntax error: Unknown command '".$command[0]."'.\n\n";
     }
 }
