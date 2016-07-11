8435fef1a1955be9c750dbf7668f473849b27732
Alexander During <alexander.during@jobleads.de>
2016-07-11 14:51:57 +0200

    Improved filtering mechanism, added support for hierarchical arrays.

--- src/Filter.php
+++ src/Filter.php
@@ -24,4 +24,32 @@ class Filter
 
         return $isMatch;
     }
+
+
+
+    private function matchesArray($filterArray, $entryArray)
+    {
+        $isMatch = true;
+        foreach ($filterArray as $key => $value) {
+            if (array_key_exists($key, $entryArray)) {
+                $entryValue = $entryArray[$key];
+            } else {
+                $isMatch = false;
+                break;
+            }
+
+            if (is_array($value) && !is_array($entryValue) || !is_array($value) && is_array($entryValue)) {
+                $isMatch = false;
+                break;
+            }
+
+            if (is_array($value) && is_array($entryValue)) {
+                $isMatch = $isMatch && $this->matchesArray($value, $entryValue);
+            } else {
+                $isMatch = $isMatch && $value === $entryValue;
+            }
+        }
+
+        return $isMatch;
+    }
 }
