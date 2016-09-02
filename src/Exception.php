fd314f30968daf6dd1caaa41945cd09fd998eacb
Alexander During <alexanderduring.git@gmail.com>
2016-09-02 13:10:18 +0200

    Added license header to every file.

--- src/Exception.php
+++ src/Exception.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * Ember Db - An embeddable document database for php.
+ * Copyright (C) 2016 Alexander During
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://github.com/alexanderduring/php-ember-db
+ * @copyright Copyright (C) 2016 Alexander During
+ * @license   http://www.gnu.org/licenses GNU General Public License v3.0
+ */
 
 namespace EmberDb;
 
